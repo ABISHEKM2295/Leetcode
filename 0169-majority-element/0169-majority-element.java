fed2fc06704c42c54dc5994a9b0fa3e916359ada
ABISHEK MAHESH <abishekmahesh.23cse@kongu.edu>
2026-03-29 11:56:01 +0530

    Time: 2 ms (70.48%), Space: 55.6 MB (57.1%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int can=nums[0];
+        int c=0;
+        for(int n:nums){
+            if(c==0) can=n;
+            if(can==n) c++;
+            else c--;
+        }return can;
+    }
+}
