69001e5488f74f28676e0831249da3826be88cdf
ABISHEK MAHESH <abishekmahesh.23cse@kongu.edu>
2026-05-08 21:21:56 +0530

    Time: 20 ms (13.7%), Space: 49.8 MB (35.97%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,22 +1,22 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if(strs==null || strs.length==0){
-            return new ArrayList<>();
-        }
+        if(strs==null || strs.length==0) return new ArrayList<>();
         Map<String,List<String>> map=new HashMap<>();
-        for(String s:strs){
-           int[] count=new int[26];
-            for(char ch:s.toCharArray()){
+        for(String s : strs){
+            int[] count=new int[26];
+            for(int i=0;i<s.length();i++){
+                char ch=s.charAt(i);
                 count[ch-'a']++;
-          }
-          StringBuilder sb=new StringBuilder();
-          for(int i=0;i<26;i++){
-            sb.append(count[i]).append("#");
-          }
-           String val=sb.toString();
-           if(!map.containsKey(val)){
-            map.put(val,new ArrayList<>());
-           } map.get(val).add(s);
+            }
+            StringBuilder sb=new StringBuilder();
+            for(int i=0;i<26;i++){
+                sb.append(count[i]).append("#");
+            }
+            String val=sb.toString();
+            if(!map.containsKey(val)){
+                map.put(val,new ArrayList<String>());
+            }
+            map.get(val).add(s);
         }return new ArrayList<>(map.values());
     }
 }
