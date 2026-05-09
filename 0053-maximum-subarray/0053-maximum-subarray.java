a446175eefff092da4169ad268ed84d98a68157b
ABISHEK MAHESH <abishekmahesh.23cse@kongu.edu>
2026-05-09 18:33:17 +0530

    Time: 1 ms (99.97%), Space: 77 MB (81.78%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max=nums[0],sum=0;
+        for(int num:nums){
+            sum+=num;
+            max=Math.max(max,sum);
+            if(sum<0) sum=0;
+        }return max;
+    }
+}
