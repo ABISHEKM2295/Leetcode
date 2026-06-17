4451844e40032d96df85b7f433e72c2f8abb9dae
ABISHEK MAHESH <abishekmahesh.23cse@kongu.edu>
2026-06-17 18:51:36 +0530

    Time: 28 ms (88.46%), Space: 47.8 MB (65.38%) - LeetHub

---
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public char processStr(String s, long k) {
+        long len=0;
+        for(char ch:s.toCharArray()){
+            if(ch=='*'){
+                if(len > 0) len--;
+            }else if(ch=='#'){
+                len*=2;
+            }else if(ch=='%'){
+                len=len;
+            }else{
+                len++;
+            }
+        }
+        if(k>=len) return '.';
+        for(int i=s.length()-1;i>=0;i--){
+            char ch=s.charAt(i);
+            if(ch=='#'){
+                len/=2;
+                if(k>=len) k-=len;
+            }else if(ch=='%'){
+                k=len-1-k;
+            }else if(ch=='*'){
+                len++;
+            }else{
+                if(k==len-1) return ch;
+                len--;
+            }
+        }return '.';
+    }
+}
