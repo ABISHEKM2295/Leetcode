c37402136e3e9e0615b43cfd515cd6bfdd828006
ABISHEK MAHESH <abishekmahesh.23cse@kongu.edu>
2026-08-24 13:56:26 +0530

    Time: 1221 ms (39.83%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,13 +1,12 @@
 # Write your MySQL query statement below
 with cte as (
-    select e.name as Employee , e.salary , e.departmentId ,
-    dense_rank() over(
+    select e.name as Employee , e.salary , e.departmentId  , dense_rank() over (
         partition by departmentId 
-        order by salary desc
-    ) as rnk
-    from Employee e
+        order by salary desc 
+    ) as rn
+from Employee e
 )
 select d.name as Department,c.Employee as Employee ,c.salary as Salary
 from cte c
 left join  Department d on c.departmentId= d.id
-where c.rnk <=3; 
+where c.rn <=3
